440a9d2206d4284034a097d538270273dd67b801
Griver2006 <ramzan.elikhanov@gmail.com>
2024-09-26 04:23:27 +0300

    fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,3 @@
-import base64
-
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -36,17 +33,6 @@ class FollowSerializer(serializers.ModelSerializer):
                 'Вы не можете подписаться на самого себя.'
             )
         return value
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super().to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
